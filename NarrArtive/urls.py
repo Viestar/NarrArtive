33cff31a580de5838175ba42b55ba7b218f29db0
Viestar <m.silverbuckson@gmail.com>
2023-10-25 10:46:24 +0300

    Backend configuration for Login, Signup, Support, Privacy Policy etc

--- NarrArtive/urls.py
+++ NarrArtive/urls.py
@@ -16,8 +16,65 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from stories.views import *
+from .views import (
+    HomeView,
+    TermsView,
+    PrivacyView,
+    SupportView,
+    SiteMapView,
+    RobotsView
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('stories.urls'))
+    path(
+        # Cleared
+        route='',
+        view=HomeView.as_view(),
+        name="home"
+    ),
+    path(
+        # Cleared
+        route='terms-of-service/',
+        view=TermsView.as_view(),
+        name="tos"
+    ),
+    path(
+        route='privacy-policy/',
+        view=PrivacyView.as_view(),
+        name="privacy"
+    ),
+    path(
+        route='support/',
+        view=SupportView.as_view(),
+        name="support"
+    ),
+    path(
+        route='sitemap.xml',
+        view=SiteMapView.as_view(),
+        name="sitemap"
+    ),
+    path(
+        route='robots.txt',
+        view=RobotsView.as_view(),
+        name="robots"
+    ),
+    # path(
+    #     route='stripe/',
+    #     view=include(
+    #         "djstripe.urls",
+    #         namespace="djstripe"
+    #     ),
+    # ),
+    path(
+        route='auth/',
+        view=include('allauth.urls')
+    ),
+    path(
+        route='admin/',
+        view=admin.site.urls,
+    ),
+    # path(
+    #     route='captcha/',
+    # ),
 ]
